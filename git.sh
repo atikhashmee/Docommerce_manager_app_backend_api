2216ae14b6923819e30db1c343144c3398748704
atik bin hashmee <atikhashmee6235@gmail.com>
2022-01-31 14:38:03 +0600

    updates....

--- git.sh
+++ git.sh
@@ -1,4 +1,31 @@
+#git add 
 git add .
-git commit -am'update'
-git pull orign master
-git push origin master
+while getopts b:c: flag
+do 
+    case "${flag}" in
+        b) branch="${OPTARG}";;
+        c) comment="${OPTARG}";;
+    esac
+done
+
+#commit
+if [ -z $comment ]
+then
+   git commit -m "updates...."
+else 
+    git commit -m "${comment}"
+fi
+#pull
+if [ -z $branch ]
+then
+    git pull origin master
+else 
+    git pull origin $branch
+fi
+#push
+if [ -z $branch ]
+then
+    git push origin master
+else 
+    git push origin $branch
+fi
